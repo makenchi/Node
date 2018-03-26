32b04c5a89bac2d2d10c927d8569ddb1e1a15f54
Lucas Silva dos Santos <lucas.santos9322@gmail.com>
2018-03-26 01:10:21 -0300

    Criando o tratamento das requisições

--- teste-node/produtos.js
+++ teste-node/produtos.js
@@ -1,1 +1,13 @@
-console.log("Listagem de produtos")
+var http = require('http');
+
+var server = http.createServer(function(request,response){
+    if(request.url == "/produtos"){
+        response.end("<html><body><h1>Listando os produtos</h1></body></html>");
+    }
+    else{
+        response.end("<html><body><h1>Home</h1></body></html>");
+    }  
+});
+server.listen(3000);
+
+console.log("Servidor online!");
